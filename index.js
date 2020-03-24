806c9848a0b1529e13e05d7a0bbc500b89f147e6
mdfaizan7 <faizaanazim@gmail.com>
2020-03-24 21:05:05 +0530

    set up database and server

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
+const { MONGODB } = require("./config");
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    userName: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
-const { MONGODB } = require("./config");
-
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World!!"
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 };
 
@@ -24,7 +38,7 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("MONGODB Connected");
+    console.log("MongoDB Connected");
     return server.listen({ port: 5000 });
   })
   .then(res => {
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  userName: String,
+  password: String,
+  email: String,
+  createdAt: String
+});
+
+module.exports = model("User", userSchema);
