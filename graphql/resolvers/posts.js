acf242b96cceaf8fb570d6715e4299af890bbb9b
mdfaizan7 <faizaanazim@gmail.com>
2020-04-02 23:19:51 +0530

    added getPost

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,6 +9,19 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     }
   }
 };
